67709bd612989843260b67573065e9cd6dc2d0ca
melonpeel <wmelonpeel@gmail.com>
2026-09-13 15:00:50 +0800

    发布 0.5.8 版本

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.alphapi.codexremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.5.7"
+        versionCode = 22
+        versionName = "0.5.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
